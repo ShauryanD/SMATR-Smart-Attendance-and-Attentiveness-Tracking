6b2f1f4da93141fac24e2414e3c1134a514dff36
ShauryanD <sndave18@gmail.com>
2024-07-22 13:54:11 -0400

    Made the attendance system and Database

---
+++ Attendance system/static/css/styles.cc
@@ -0,0 +1,25 @@
+body {
+    font-family: Arial, sans-serif;
+}
+
+h1 {
+    text-align: center;
+}
+
+table {
+    width: 100%;
+    border-collapse: collapse;
+}
+
+th, td {
+    padding: 10px;
+    text-align: left;
+}
+
+th {
+    background-color: #f2f2f2;
+}
+
+tr:nth-child(even) {
+    background-color: #f9f9f9;
+}
